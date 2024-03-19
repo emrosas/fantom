64965019aaba2726af1012fda2a1de97be2bddd9
Erick Mireles <emireles.rosas@gmail.com>
2024-03-19 13:48:30 -0600

    Add project information in projects.ts

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -1,20 +1,63 @@
+export interface Project {
+  id: number;
+  name: string;
+  description: string;
+  tags: string[];
+}
+
 export const projects = [
   {
     id: 1,
-    name: "Project 1",
+    name: "La Postreria 77",
     description:
-      "Lorem ipsum dolor sit amet consectetur. Lectus ipsum enim porttitor fermentum maecenas rhoncus at suspendisse. Sit vel gravida commodo mi morbi eget nibh ut nunc. Pellentesque commodo sed dignissim aliquam euismod.",
+      "The La Postreria 77 website was built using Webflow and its CMS solution. The website is fully responsive and features a dynamic menu solution that allows the client to easily update the menu items and prices. This feature alse enables them to divide their menu into multiple branches by utilizing stylized and reusable components.",
+    tags: [
+      "Web Design",
+      "Web Development",
+      "Webflow",
+      "Javascript",
+      "CMS",
+      "Dynamic Menu Solution",
+    ],
   },
   {
     id: 2,
-    name: "Project 2",
+    name: "The Wrap Co.",
     description:
-      "Lorem ipsum dolor sit amet consectetur. Lectus ipsum enim porttitor fermentum maecenas rhoncus at suspendisse.",
+      "This is a fully responsive website featuring static and dynamic elements throughtout the site. Built with Astro for it's unmatched speed and React for it's dynamic capabilities.",
+    tags: [
+      "Web Design",
+      "Web Development",
+      "Astro JS",
+      "Typescript",
+      "Dynamic Menu Solution",
+    ],
   },
   {
     id: 3,
-    name: "Project 3",
+    name: "La Pasteleria de La Postreria 77",
     description:
-      "Lorem ipsum dolor sit amet consectetur. Lectus ipsum enim porttitor fermentum maecenas rhoncus at suspendisse. Sit vel gravida commodo mi morbi eget nibh ut nunc. Pellentesque commodo sed dignissim aliquam euismod. Lorem ipsum dolor sit amet consectetur. Lectus ipsum enim porttitor fermentum maecenas rhoncus at suspendisse. Sit vel gravida commodo mi morbi eget nibh ut nunc. Pellentesque commodo sed dignissim aliquam euismod.",
+      "This simple website is both elegant and informative, it aims to provide the user with a quick and easy way to find the location of the bakery and to contact them.",
+    tags: ["Web Design", "Web Development", "Astro JS", "React"],
+  },
+  {
+    id: 4,
+    name: "Party Game Companion",
+    description:
+      "This is a simple, fully responsive web application that allows the user to keep track of score and displays a leaderboard to be used when playing party games with friends.",
+    tags: ["UX/UI Design", "Web Development", "React", "Vite", "Zustand"],
+  },
+  {
+    id: 5,
+    name: "Herrmat",
+    description:
+      "Herrmat's site features an integrated search and filter solution that allows the user to easily find the product they are looking for. The site is fully responsive and features a dynamic catalog solution that allows the client to easily update the catalogs items and prices.",
+    tags: [
+      "Web Design",
+      "Web Development",
+      "Webflow",
+      "CMS",
+      "Dynamic Catalog",
+    ],
   },
 ];
